d5d935cfb810b54fefaa089217e18a724c994ff9
willem <willem@dotready.nl>
2016-06-26 11:41:25 +0200

    update on readme and example

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # php-unionplatform
 A PHP Client library for the UnionPlatform
+
+
+### purpose ###
+
+Although it is a unionplatform client, it is not a fully functioning socketserver client.
+At this point it assumes that the server accepts every message and does not do any
+level of server response checking.
+
+It's purpose is just sending the right message to the server through http or https.
+Unionplatform makes use of long polling and expects the connection to stay open
+
+
+### contributing ###
+
+I know this is far from perfect but it will do it's job. But please feel free to contribute.
--- example.php
+++ example.php
@@ -5,11 +5,11 @@ require "vendor/autoload.php";
 use phpunionplatform\service\Unionplatform;
 use phpunionplatform\library\client\HttpsClient;
 
-$host = 'ipaddr';
-$port = 80;
-$domain = 'yourdomain.com';
+$host = 'example.com';
+$port = 9200;
+$domain = 'example.com';
 
-$room = 'awesomeRoomName';
+$room = 'systemChannel';
 
 // create new http interface
 $httpClient = new HttpsClient($host, $port, $domain);
@@ -30,4 +30,4 @@ $unionplatformService->joinRoom($room);
 
 // send a room message, if the userid is set it will send only to that user
 // the last two parameters (userId, params[]) are optional
-$unionplatformService->sendMessage($room, 'NOTIFY', '', array('hello my friend!'));
+$unionplatformService->sendMessage($room, 'NOTIFY', 17, array('hello my friend!'));
